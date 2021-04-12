dfa36eeda23cd0941240fa270c97058b3f10a249
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2021-04-12 00:46:13 -0700

    added rectangle and triang;e

--- app/src/main/java/com/hanseltritama/canvasapi/MainActivity.java
+++ app/src/main/java/com/hanseltritama/canvasapi/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.view.View;
 
@@ -27,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
             super.onDraw(canvas);
 
             Paint paint = new Paint();
+            Path path = new Path();
 
             // circle
             int x = getWidth();
@@ -41,6 +44,26 @@ public class MainActivity extends AppCompatActivity {
             paint.setColor(Color.parseColor("#FFFFFF"));
 
             canvas.drawCircle(x/2, y/2, radius, paint);
+
+            // rectangle
+            paint.setColor(Color.parseColor("#FF0000"));
+
+            canvas.drawRect(100,50,300,300, paint);
+
+            // triangle
+            Point a = new Point(350, 50);
+            Point b = new Point(350, 300);
+            Point c = new Point(550, 50);
+
+            path.setFillType(Path.FillType.EVEN_ODD);
+
+            path.lineTo(a.x, a.y);
+            path.lineTo(b.x, b.y);
+            path.lineTo(c.x, c.y);
+            path.lineTo(a.x, a.y);
+            path.close();
+
+            canvas.drawPath(path, paint);
         }
     }
 }
